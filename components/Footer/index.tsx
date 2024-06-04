b04529c93bd090d1d7a04511f9b7b6431874835f
thoriqdharmawan <thoriqd21@gmail.com>
2024-06-05 00:32:30 +0700

    feat(homepage): add footer section

---
+++ components/Footer/index.tsx
@@ -0,0 +1,28 @@
+export default function Footer() {
+  return (
+    <div className="bg-slate-100 pt-20">
+      <div className="bg-slate-800 flex flex-col items-center">
+        <div className="bg-gradient-to-r from-primary to-violet-400 max-w-screen-lg w-full p-16 rounded-xl -mt-12 mb-24">
+          <h3 className="font-extrabold text-2xl text-secondary">
+            Let's Make Simply Your Digital Adoption
+          </h3>
+        </div>
+      </div>
+
+      <div className="pb-24 bg-slate-800 flex justify-center">
+        <div className="grid grid-cols-2 gap-6 w-full max-w-screen-lg">
+          <div>
+            <h2 className="font-extrabold text-xl text-secondary">
+              META Vision
+            </h2>
+          </div>
+          <div>
+            <h2 className="font-extrabold text-xl text-secondary">
+              META Vision
+            </h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- components/Products/index.tsx
+++ components/Products/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ProductItem from "./ProductItem";
 import { Button } from "../ui/button";
 
